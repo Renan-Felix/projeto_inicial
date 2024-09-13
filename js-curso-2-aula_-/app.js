95dd5b045688f8df309e15c0a9530256e90e6751
Renan Félix <renan.felix25@hotmail.com>
2024-09-13 13:36:24 -0300

    Modificando o número limite para 100

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 150;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativa = 1;
 
@@ -18,7 +18,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 150');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
 }
 exibirMensagemInicial();
